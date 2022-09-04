6e31d7aefc6a48332dd395dae24d1a22d8e22f82
SalvadorFloresRivero <salvador.flores@insabi.gob.mx>
2022-09-03 23:16:28 -0500

    Implementación de Dashboard covid 2

--- js/main.js
+++ js/main.js
@@ -1,26 +1,34 @@
 const currencyDate = document.getElementById("currency-date")
 const btnDate = document.getElementById("btn-date")
-var ctx2 = document.getElementById('myChart2').getContext('2d');
-var ctx3 = document.getElementById('myChart3').getContext('2d');
-var ctx4 = document.getElementById('myChart4').getContext('2d');
+var ctx2 = document.getElementById('myChartLines').getContext('2d');
+var ctx3 = document.getElementById('myChartBar').getContext('2d');
+//var ctx4 = document.getElementById('myChart4').getContext('2d');
 
 const state = {
     label: "",
     labels: [],
-    dataValues: []
+    dataValues: [],
+    casosConf_2020: 0,
+    casosConf_2021: 0,
+    casosConf_2022: 0,
+    casosDef: 0,
+    casosRec:0
 }
 
 
 async function fetchData(date){
     //console.log(date)
-    labels = []
-    dataValues = []
+    let labels = []
+    let dataValues = []
+    let casosConf_2020 = 0
+    let casosConf_2021 = 0
+    let casosConf_2022 = 0
+    let casosDef = 0
+    let casosRec =0
+    let fecha_corta = ''
+    let f_anio = ''
 
- //return fetch(`https://data.fixer.io/api/${dateToLookFor}?access_key=${key}&base=EUR&symbols=USD,GBP,MXN,CAD,ERN,BYN,CUP`).then((res) => {
-    const response = await fetch(`https://api.covid19api.com/total/dayone/country/mexico/status/confirmed`)
-    //const data = await response.json()
-    //console.log(data)
-  //return data
+     const response = await fetch(`https://api.covid19api.com/total/dayone/country/mexico/status/confirmed`)
      .then((response) => {
         const data = response.json()
         //console.log(data)
@@ -28,22 +36,43 @@ async function fetchData(date){
     }).then(data => {
 
         for(let i=0; i < data.length ; i++){
-            labels.push(data[i].Date)
-            //console.log(data[i].Cases)
-            dataValues.push(data[i].Cases)
-           // console.log(data[i].Date)
+            f_anio = data[i].Date.substring(0,4)
+
+            if( f_anio == '2020' ){
+                //fecha_corta = data[i].Date.substring(9,10) + data[i].Date.substring(4,7)+'-'+f_anio
+                //dataValues.push(data[i].Cases) // Casos acumulados Confirmados para gráfica de lineas
+                casosConf_2020+= data[i].Cases // Casos acumulados Confirmados para gráfica de barras
+                //labels.push(fecha_corta)
+            }
+            if( f_anio == '2021' ){
+                //fecha_corta = data[i].Date.substring(9,10) + data[i].Date.substring(4,7)+'-'+f_anio
+                //dataValues.push(data[i].Cases) // Casos acumulados Confirmados para gráfica de lineas
+                casosConf_2021+= data[i].Cases // Casos acumulados Confirmados para gráfica de barras
+                //labels.push(fecha_corta)
+            }
+            if( f_anio == '2022' ){
+                fecha_corta = data[i].Date.substring(9,10) + data[i].Date.substring(4,7)+'-'+f_anio
+                dataValues.push(data[i].Cases) // Casos acumulados Confirmados para gráfica de lineas
+                casosConf_2022+= data[i].Cases // Casos acumulados Confirmados para gráfica de barras
+                labels.push(fecha_corta)
+            }
+
+
         }
 
         state.label = "Casos confirmados"
         state.labels = Object.values(labels)
         state.dataValues = Object.values(dataValues)
-        console.log(Object.values(labels))
-        console.log(Object.values(dataValues))
+        state.casosConf_2020=casosConf_2020
+        state.casosConf_2021=casosConf_2021
+        state.casosConf_2022=casosConf_2022
+        //console.log(Object.values(labels))
+        //console.log(Object.values(dataValues))
     })
 }
 
 function generateChart(dataLabel, dataLabels, dataValues){
-    console.log(dataLabels)
+    //console.log(dataLabels)
     let myChart = new Chart(ctx2, {
         type: 'line',
         data: {
@@ -103,23 +132,36 @@ function generateChart(dataLabel, dataLabels, dataValues){
     });
 }
 // Muertes, Recuperados y Confirmados
-function generateChartBarras(dataLabel, dataLabels, dataValues){
-    console.log(dataLabels)
+function generateChartBarras(dataLabel, dataValues_2020, dataValues_2021, dataValues_2022){
+    //console.log(dataLabels)
 
-     var myChart3 = new Chart(ctx3, {
-    type: 'line',
+    var myChart3 = new Chart(ctx3, {
+    type: 'bar',
     data: {
-      labels: [        'Sunday',        'Monday',        'Tuesday',        'Wednesday',        'Thursday',        'Friday',        'Saturday'      ],
+      labels: ['2020','2021','2022'],
       datasets: [{
-        data: [          15339,          21345,          18483,          24003,          23489,          24092,          12034        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: 'green',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
+        data: [dataValues_2020, dataValues_2021, dataValues_2022],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)'
+        ],
+        borderWidth: 1
       }]
     },
     options: {
+        title: {
+            display: true,
+            text: 'Casos de reporte COVID-19',
+            fontSize: 20,
+            padding: 30,
+            fontColor: '#12619c'
+        },
       scales: {
         yAxes: [{
           ticks: {
@@ -168,7 +210,7 @@ function generateChartBarras(dataLabel, dataLabels, dataValues){
 const init = async () => {
     await fetchData()
     await generateChart(state.label,state.labels,state.dataValues)
-    await generateChartBarras(state.label,state.labels,state.dataValues)
+    await generateChartBarras(state.label, state.casosConf_2020, state.casosConf_2021, state.casosConf_2022)
 }
 
 init()
